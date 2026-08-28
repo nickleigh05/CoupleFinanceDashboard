becb5bf5108be98d643c68e553621de3ee8107b3
nickleigh <nickleigh05@icloud.com>
2026-08-27 19:06:44 -0700

    Frontend

--- backend/CoupleFinanceDashboard.Api/Controllers/DashboardController.cs
+++ backend/CoupleFinanceDashboard.Api/Controllers/DashboardController.cs
@@ -18,12 +18,42 @@ public class DashboardController : ControllerBase
     [HttpGet]
     public IActionResult Get()
     {
-
         List<Account> accounts = _context.Accounts.ToList();
         List<Partner> partners = _context.Partners.ToList();
 
         decimal CombinedNetWorth = accounts.Sum(account => account.Balance);
 
-    return Ok(CombinedNetWorth);
+        List<object> partnerBreakdowns = new List<object>();
+
+        foreach (Partner partner in partners)
+        {
+            var partnerAccounts = accounts
+                .Where(account => account.PartnerId == partner.Id)
+                .Select(account => new
+                {
+                    id = account.Id,
+                    institutionName = account.InstitutionName,
+                    accountName = account.AccountName,
+                    type = account.Type,
+                    balance = account.Balance
+                })
+                .ToList();
+
+            decimal partnerNetWorth = partnerAccounts.Sum(account => account.balance);
+
+            partnerBreakdowns.Add(new
+            {
+                partnerId = partner.Id,
+                partnerName = partner.Name,
+                netWorth = partnerNetWorth,
+                accounts = partnerAccounts
+            });
+        }
+
+        return Ok(new
+        {
+            combinedNetWorth = CombinedNetWorth,
+            partners = partnerBreakdowns
+        });
     }
 }
